f35be81d23cfc3b16b513413ffba8a65367f4512
gamestar <rinayumiho@gmail.com>
2021-07-15 14:41:55 -0400

    Project_1 done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_courses,
+        through: :enrollments,
+        source: :course
+        # primary_key: :id,
+        # foreign_key: :course_id,
+        # class_name: :Course
+
+    has_many :lectures,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :Course
+
 end
